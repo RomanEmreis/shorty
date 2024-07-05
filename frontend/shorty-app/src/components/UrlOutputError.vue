21a0ae375740a08789deee471678b1b72bd6dacd
Roman Emreis <Roman_Emreis@epam.com>
2024-07-05 23:11:35 +0400

    decomposite UrlOutput component

---
+++ frontend/shorty-app/src/components/UrlOutputError.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  error: string
+}>();
+</script>
+
+<template>
+  <div class='error-field'>
+    <span>{{ error }}</span>
+  </div>
+</template>
+
+<style scoped>
+.error-field {
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: row;
+  background-color: transparent;
+  border: 1px solid #fb5c56;
+  border-radius: 5px;
+  height: 4rem;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+}
+
+.error-field span {
+  margin-left: 1.5rem;
+  font-size: 1.5rem;
+  color:#fb5c56;
+}
+</style>
---
+++ frontend/shorty-app/src/components/UrlOutputResult.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+const props = defineProps<{
+  url: string
+}>();
+
+const handleCopy = () => navigator.clipboard.writeText(props.url);
+</script>
+
+<template>
+  <div class='result-field'>
+    <a :href="url" target='_blank'>{{ url }}</a>
+    <button class='copy-btn' @click.prevent="handleCopy">
+      <img alt="copy-img" src="./icons/copy.svg" width="38" height="38" />
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.result-field {
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: row;
+  background-color: transparent;
+  border: 1px solid hsla(160, 100%, 37%, 1);
+  border-radius: 5px;
+  height: 4rem;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+}
+
+.result-field a {
+  margin-left: 1.5rem;
+  font-size: 1.5rem;
+  color:hsla(160, 100%, 37%, 1);
+}
+
+.copy-btn {
+  position: relative;
+  margin-right: 0.5rem;
+  height: 72%;
+  width: 4rem;
+  cursor: pointer;
+  background: transparent;
+  border-radius: 5px;
+  border: 0px solid transparent;
+  opacity: .8;
+}
+
+.copy-btn:hover {
+  opacity: .4;
+}
+
+.copy-btn img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
