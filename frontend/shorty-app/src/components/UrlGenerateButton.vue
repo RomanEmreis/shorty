0246e7e424ddfe32ba0ab2ba610db6d5cc6b500e
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-07-07 16:57:04 +0400

    added dark/light theme support (#18)
    * added dark/light theme support
    
    * fixed test
    
    ---------
    
    Co-authored-by: Roman Emreis <Roman_Emreis@epam.com>

---
+++ frontend/shorty-app/src/components/UrlGenerateButton.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core';
+
+defineProps<{
+  handleClick: () => Promise<void>
+}>();
+  
+const mode = useColorMode();
+</script>
+
+<template>
+  <button class='generate-btn' @click.prevent="handleClick">
+    <img v-if="mode === 'dark'" alt="generate-img" src="./icons/generate.svg" width="38" height="38" />
+    <img v-if="mode === 'light'" alt="generate-img" src="./icons/generate-dark.svg" width="38" height="38" />
+  </button>
+</template>
+
+<style scoped>
+.generate-btn {
+  position: relative;
+  height: 72%;
+  width: 4rem;
+  cursor: pointer;
+  margin-right: 0.5rem;
+  margin-left: 1rem;
+  background: transparent;
+  border-radius: 5px;
+  border: 0px solid transparent;
+  opacity: .7;
+}
+
+.generate-btn:hover {
+  opacity: .4;
+}
+
+.generate-btn img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
--- frontend/shorty-app/src/components/__tests__/UrlShortener.spec.ts
+++ frontend/shorty-app/src/components/__tests__/UrlShortener.spec.ts
@@ -5,21 +5,27 @@ import UrlShortener from '../UrlShortener.vue'
 import { useUrlStore } from '@/stores/url';
 
 const defaultExpectedHtml = `<div data-v-8174189b="" class="center-screen">
-  <div data-v-f3006f2f="" data-v-8174189b="" class="url-input"><input data-v-f3006f2f="" type="text" placeholder="https://www.very-long-url.com" value=""><button data-v-f3006f2f="" class="generate-btn"><img data-v-f3006f2f="" alt="generate-img" src="/src/components/icons/generate.svg" width="38" height="38"></button></div>
+  <div data-v-f3006f2f="" data-v-8174189b="" class="url-input"><input data-v-f3006f2f="" type="text" placeholder="https://www.very-long-url.com" value=""><button data-v-6c14b1e7="" data-v-f3006f2f="" class="generate-btn">
+      <!--v-if--><img data-v-6c14b1e7="" alt="generate-img" src="/src/components/icons/generate-dark.svg" width="38" height="38">
+    </button></div>
   <!--v-if-->
   <!--v-if-->
   <div data-v-8174189b="" class="copyright">Created by Roman Emreis</div>
 </div>`;
 
 const withShortUrlExpectedHtml = `<div data-v-8174189b="" class="center-screen">
-  <div data-v-f3006f2f="" data-v-8174189b="" class="url-input"><input data-v-f3006f2f="" type="text" placeholder="https://www.very-long-url.com" value=""><button data-v-f3006f2f="" class="generate-btn"><img data-v-f3006f2f="" alt="generate-img" src="/src/components/icons/generate.svg" width="38" height="38"></button></div>
+  <div data-v-f3006f2f="" data-v-8174189b="" class="url-input"><input data-v-f3006f2f="" type="text" placeholder="https://www.very-long-url.com" value=""><button data-v-6c14b1e7="" data-v-f3006f2f="" class="generate-btn">
+      <!--v-if--><img data-v-6c14b1e7="" alt="generate-img" src="/src/components/icons/generate-dark.svg" width="38" height="38">
+    </button></div>
   <div data-v-aadec475="" class="result-field"><a data-v-aadec475="" href="http://something" target="_blank">http://something</a><button data-v-aadec475="" class="copy-btn"><img data-v-aadec475="" alt="copy-img" src="/src/components/icons/copy.svg" width="38" height="38"></button></div>
   <!--v-if-->
   <div data-v-8174189b="" class="copyright">Created by Roman Emreis</div>
 </div>`;
 
 const withErrorExpectedHtml = `<div data-v-8174189b="" class="center-screen">
-  <div data-v-f3006f2f="" data-v-8174189b="" class="url-input"><input data-v-f3006f2f="" type="text" placeholder="https://www.very-long-url.com" value=""><button data-v-f3006f2f="" class="generate-btn"><img data-v-f3006f2f="" alt="generate-img" src="/src/components/icons/generate.svg" width="38" height="38"></button></div>
+  <div data-v-f3006f2f="" data-v-8174189b="" class="url-input"><input data-v-f3006f2f="" type="text" placeholder="https://www.very-long-url.com" value=""><button data-v-6c14b1e7="" data-v-f3006f2f="" class="generate-btn">
+      <!--v-if--><img data-v-6c14b1e7="" alt="generate-img" src="/src/components/icons/generate-dark.svg" width="38" height="38">
+    </button></div>
   <!--v-if-->
   <div data-v-5427ef97="" class="error-field"><span data-v-5427ef97="">some error</span></div>
   <div data-v-8174189b="" class="copyright">Created by Roman Emreis</div>
