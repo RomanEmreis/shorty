1a5b610eb8117eab78f04442fe8712f1a32f40dd
Roman <39233213+RomanEmreis@users.noreply.github.com>
2024-07-07 19:22:34 +0400

    theme toggle now correctly shows on auto mode (#20)
    Co-authored-by: Roman Emreis <Roman_Emreis@epam.com>

--- frontend/shorty-app/src/components/ThemeToggle.vue
+++ frontend/shorty-app/src/components/ThemeToggle.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-import { useColorMode, useCycleList } from '@vueuse/core';
+import { useColorMode, useCycleList, usePreferredColorScheme } from '@vueuse/core';
 import { watchEffect } from 'vue-demi';
+import { computed } from 'vue'
 
 const mode = useColorMode({ emitAuto: true });
 const { state, next } = useCycleList(['dark', 'light'] as const, { initialValue: mode })
+const prefferedMode = usePreferredColorScheme();
+const currentMode = computed(() => mode.value === 'auto' ? prefferedMode.value : mode.value);
 
 watchEffect(() => mode.value = state.value);
 </script>
 
 <template>
   <button class="theme-toggle" @click="next()">
-    <img v-if="mode === 'dark'" alt="dark-mode-img" src="./icons/dark-mode.svg" width="22" height="22" />
-    <img v-if="mode === 'light'" alt="light-mode-img" src="./icons/light-mode.svg" width="26" height="26" />
+    <img v-if="currentMode === 'dark'" alt="dark-mode-img" src="./icons/dark-mode.svg" width="22" height="22" />
+    <img v-if="currentMode === 'light'" alt="light-mode-img" src="./icons/light-mode.svg" width="26" height="26" />
   </button>
 </template>
 
